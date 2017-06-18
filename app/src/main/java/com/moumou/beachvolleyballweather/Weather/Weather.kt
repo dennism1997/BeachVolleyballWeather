512ab8d571ce64b065a5f8b17f27142aabb6cab4
Dennis Mouwen <dennismouwen@gmail.com>
2017-06-18 21:27:28 +0200

    added windfactor and some tests

--- app/src/main/java/com/moumou/beachvolleyballweather/Weather/Weather.kt
+++ app/src/main/java/com/moumou/beachvolleyballweather/Weather/Weather.kt
@@ -1,6 +1,11 @@
 package com.moumou.beachvolleyballweather.Weather
 
-data class Weather(val summary : String, val temperature : Double, val precipProb : Double, val windSpeed : Double) {
+import java.lang.Math.pow
+
+data class Weather(val summary : String,
+                   val temperature : Double,
+                   val precipProb : Double,
+                   val windSpeed : Double) {
 
     val weatherResult : Double
     val possible : Boolean
@@ -13,24 +18,33 @@ data class Weather(val summary : String, val temperature : Double, val precipPro
         windFactor = calculateWindFactor()
         precipFactor = calculatePrecipFactor()
         weatherResult = calculateWeather()
-        possible = weatherResult > WeatherCalculator.Threshold
+        possible = calculatePossible()
     }
 
     fun calculateWeather() : Double {
-        val total : Double = .5 * temperatureFactor + .3 * windFactor + .2 * precipFactor
-        return total
+        return .5 * temperatureFactor + .3 * windFactor + .2 * precipFactor
     }
 
     fun calculateTemperatureFactor() : Double {
-        return ((-0.0215 * Math.pow(temperature, 3.0)) + 0.8754 * Math.pow(temperature, 2.0) + (-4.8251 * temperature) + 7.7724) / 100
+        return ((-0.0215 * pow(temperature, 3.0)) + 0.8754 * pow(temperature,
+                                                                 2.0) + (-4.8251 * temperature) + 7.7724) / 100
     }
 
     fun calculateWindFactor() : Double {
-        return 1.0
+        return (0.0162 * pow(windSpeed, 3.0)) + (-0.0445 * pow(windSpeed,
+                                                               2.0)) + (-9.9975 * windSpeed) + 100.0416
     }
 
     fun calculatePrecipFactor() : Double {
-        return (0.0003 * Math.pow(precipProb, 3.0) + (-0.052 * Math.pow(precipProb, 2.0)) + 1.0299 * precipProb + 96.6506) / 100
+        return (0.0003 * pow(precipProb, 3.0) + (-0.052 * pow(precipProb,
+                                                              2.0)) + 1.0299 * precipProb + 96.6506) / 100
     }
 
+    fun calculatePossible() : Boolean {
+        if (temperature < 5.0 || windSpeed > 17.0 || precipProb > 90.0) {
+            return false
+        }
+
+        return weatherResult > WeatherCalculator.Threshold
+    }
 }
---
+++ app/src/test/java/com/moumou/beachvolleyballweather/WeatherTest.java
@@ -0,0 +1,47 @@
+package com.moumou.beachvolleyballweather;
+
+import android.support.annotation.NonNull;
+
+import com.moumou.beachvolleyballweather.Weather.Weather;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+@RunWith(Parameterized.class)
+public class WeatherTest {
+
+    @NonNull
+    @Parameterized.Parameters
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{{0.0, 0.0, 0.0, false},
+                {15, 0, 0, true},
+                {15, 100, 0, false},
+                {15, 50, 5, true},
+                {5, 0, 0, false},
+                {15, 0, 0, true}});
+    }
+
+    private final double temp;
+    private final double precip;
+    private final double wind;
+    private final boolean expected;
+
+    public WeatherTest(double temp, double precip, double wind, boolean expected) {
+        this.temp = temp;
+        this.precip = precip;
+        this.wind = wind;
+        this.expected = expected;
+    }
+
+    @Test
+    public void test() {
+        Weather w = new Weather("", temp, precip, wind);
+        Assert.assertEquals(expected, w.getPossible());
+    }
+}
+
