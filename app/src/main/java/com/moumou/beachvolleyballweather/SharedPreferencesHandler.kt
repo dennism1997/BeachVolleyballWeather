528aba088fdc4411c34cd8f93e444211af286b20
Dennis Mouwen <dennismouwen@gmail.com>
2017-06-22 18:52:31 +0200

    added better exception handling

--- app/src/main/java/com/moumou/beachvolleyballweather/SharedPreferencesHandler.kt
+++ app/src/main/java/com/moumou/beachvolleyballweather/SharedPreferencesHandler.kt
@@ -3,8 +3,10 @@ package com.moumou.beachvolleyballweather
 import android.content.Context
 import android.location.Geocoder
 import android.location.Location
+import android.support.v7.app.AlertDialog
 import android.support.v7.preference.PreferenceManager
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import com.google.gson.reflect.TypeToken
 import java.util.Locale
 import kotlin.collections.ArrayList
@@ -24,13 +26,17 @@ object SharedPreferencesHandler {
         val sharedPref = PreferenceManager.getDefaultSharedPreferences(c)
         val s = sharedPref.getString(KEY_WEATHER_LOCATIONS, "")
 
-        if(s == ""){
-            return ArrayList()
-        }
         val type = object : TypeToken<ArrayList<Location>>() {}.type
-        val list = Gson().fromJson<ArrayList<Location>>(s, type)
+        try {
+            return Gson().fromJson<ArrayList<Location>>(s, type)
+        } catch (e : JsonSyntaxException) {
+            AlertDialog.Builder(c).setMessage(c.getString(R.string.storage_parsing_error)).setPositiveButton(
+                    R.string.ok) { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+        }
 
-        return list
+        return ArrayList()
     }
 
     @Synchronized
